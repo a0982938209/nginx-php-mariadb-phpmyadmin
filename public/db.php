2d5aeee9c82057fac05dcc1c0db3bfec7619c3f9
John <a0982938209@gmail.com>
2021-09-26 20:54:32 +0800

    git init

---
+++ public/db.php
@@ -0,0 +1,12 @@
+<?php
+$conn = new mysqli("web_mariadb", "root", "pwd123", "test");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM name";
+$result = $conn->query($sql);
+while($row = $result->fetch_array()){
+    echo $row["name"];
+  }
+$conn->close();
